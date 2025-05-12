3409a4a95f1fbbc0fd3d443ad8f48509614f8e54
Elamaran472004 <elamaran472004@gmail.com>
2025-05-12 13:41:17 +0530

    Initial commit with all folders

---
+++ controllers/moviesController.js
@@ -0,0 +1,29 @@
+const {
+  getAllMovies,
+  getMovieById,
+  createMovie,
+  updateMovie,
+  deleteMovie,
+  getMovieByGenre,
+} = require("../services/movies");
+
+const getAllMoviesController = async (req, res, next) => {
+  let result= await getAllMovies();
+  res.status(200).send(result);
+};
+
+const getMovieByIdController = async (req, res, next) => {};
+const getMovieByGenreController = async (req, res, next) => {};
+
+const createMovieController = async (req, res, next) => {};
+const updateMovieController = async (req, res, next) => {};
+const deleteMovieController = async (req, res, next) => {};
+
+module.exports = {
+  getAllMoviesController,
+  getMovieByIdController,
+  getMovieByGenreController,
+  createMovieController,
+  updateMovieController,
+  deleteMovieController,
+};
---
+++ model/schema.js
@@ -0,0 +1,13 @@
+const mongoose =  require("mongoose"); 
+
+const movieSchema = new mongoose.Schema({
+    movieId: String,
+    movieName: String,
+    genre: String,
+    favorite: String,
+    rating: Number,
+    moviePoster: String,
+    moviePlot: String
+})
+const Movies = mongoose.model('movies', movieSchema)
+module.exports  = {Movies}
